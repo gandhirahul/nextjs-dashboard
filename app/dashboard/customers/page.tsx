12690ac1b19b1da4dcbbd9822658ae83ee783113
Rahul Gandhi <rahulgandhi38@gmail.com>
2024-08-19 20:29:56 +0100

    add metadata

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
+
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Customers',
+}
+
 export default function Page () {
     return <p>Welcome to the Customers page!</p>
 }
